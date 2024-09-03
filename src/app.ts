cef49371de073ecccada1f4c297c7e1a3400fe1e
Arya Wibowo Jati <aryajati29@gmail.com>
2024-09-03 14:37:57 +0700

    chore: initial commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send("Hello World");
+});
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
+
+module.exports = app;
