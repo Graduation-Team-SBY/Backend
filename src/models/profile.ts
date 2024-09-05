adffe34f421834466725c2417029bec109557a4d
Arya Wibowo Jati <aryajati29@gmail.com>
2024-09-05 19:49:28 +0700

    feat: merge dev-ariiq to dev-arya

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,14 +1,6 @@
 import { model, Schema } from "mongoose";
 import { IProfileSchema } from "../types";
 import { ObjectId } from "mongodb";
-<<<<<<< HEAD
-const profileSchema = new Schema<IProfileSchema>({
-  name: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
-  profilePicture: { type: String },
-  address: { type: String, required: true },
-  userId: { type: ObjectId, required: true },
-=======
 
 const profileSchema = new Schema<IProfileSchema>({
     name: { type: String, default: null },
@@ -16,7 +8,6 @@ const profileSchema = new Schema<IProfileSchema>({
     profilePicture: { type: String },
     address: { type: String, default: null },
     userId: ObjectId
->>>>>>> 7cf625a7069e815dbcea3de5cf1eb1bd9c29b1db
 });
 
 export const Profile = model("Profile", profileSchema);
